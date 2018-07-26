46d5ef1bada9a677d96146850314767d776b0456
antonella <antonella_caroca@hotmail.com>
2018-07-26 12:29:16 -0400

    agrego visualización de restaurantes y sus respectivas direcciones usando una tabla de Bootstrap

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
+let contenido = document.querySelector('#contenido')
+const btn = document.getElementById('obtener') 
 
+function traer(){
+  btn.style.display='none'
 function getData (method, url) {
       return new Promise(function (resolve, reject) {
        var xhr = new XMLHttpRequest()
@@ -22,26 +26,60 @@ function getData (method, url) {
         xhr.send()
       })
      }
-     
+    
      getData('GET', 'https://places.demo.api.here.com/places/v1/discover/search?at=-33.4167%2C-70.65&q=restaurant&app_id=YTwDUxFfBXp7TI2GimXS&app_code=qlKZLMttFtDmjErf3WAolA').then(function (data) {
   let restaurants = JSON.parse(data)
   console.log(restaurants)
-  let output = ''
+  contenido.innerHTML = ''
   Object.entries(restaurants)
-  
-    output += `
-    <ul>
-         <li>Title: ${restaurants.results.items[19].title}</li>
-         <li>Dirección: ${restaurants.results.items[19].vicinity}</li>
-    </ul>
-         `
-   
-           document.getElementById('template').innerHTML = output
+
+    contenido.innerHTML += `
+    <tr>
+      <td> ${restaurants.results.items[0].title}</td>
+      <td>${restaurants.results.items[0].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[1].title}</td>
+      <td>${restaurants.results.items[1].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[2].title}</td>
+      <td>${restaurants.results.items[2].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[3].title}</td>
+      <td>${restaurants.results.items[3].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[4].title}</td>
+      <td>${restaurants.results.items[4].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[5].title}</td>
+      <td>${restaurants.results.items[5].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[6].title}</td>
+      <td>${restaurants.results.items[6].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[7].title}</td>
+      <td>${restaurants.results.items[7].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[8].title}</td>
+      <td>${restaurants.results.items[8].vicinity}</td>
+    </tr>
+    <tr>
+      <td> ${restaurants.results.items[9].title}</td>
+      <td>${restaurants.results.items[9].vicinity}</td>    
+    </tr>
+  `
            xhr.open()
          }).catch(function (err) {
          })
        
         
-                  
+        }         
         
-     //  <li>Dirección: ${Object.entries(restaurants.search.context.location.address)}</li>
+    
