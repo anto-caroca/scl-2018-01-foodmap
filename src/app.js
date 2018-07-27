8aceffd6b82fb5d8de9e215dda9cf321a4de2441
antonella <antonella_caroca@hotmail.com>
2018-07-26 22:22:13 -0400

    saco tabla de restaurantes y agrego select con opciones de restaurantes. También agrego botón ver que muestra un modal

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-let contenido = document.querySelector('#contenido')
-const btn = document.getElementById('obtener') 
+const pModal = document.getElementById('pModal')
+const select = document.getElementById('foodList')
+const ver = document.getElementById('ver-btn')
+let food
 
-function traer(){
-  btn.style.display='none'
+ver.addEventListener('click', showModal)
+
+function showModal(){
+  $('#myModal').modal()
+}
+
 function getData (method, url) {
       return new Promise(function (resolve, reject) {
        var xhr = new XMLHttpRequest()
@@ -30,56 +36,27 @@ function getData (method, url) {
      getData('GET', 'https://places.demo.api.here.com/places/v1/discover/search?at=-33.4167%2C-70.65&q=restaurant&app_id=YTwDUxFfBXp7TI2GimXS&app_code=qlKZLMttFtDmjErf3WAolA').then(function (data) {
   let restaurants = JSON.parse(data)
   console.log(restaurants)
-  contenido.innerHTML = ''
-  Object.entries(restaurants)
+  
+  // let c = Array.from(restaurants.results.items) 
+  /// contenido.innerHTML += 
+ // Object.entries(c).forEach((x) => document.write(x[1].title));
+  
+for (let i = 0; restaurants.results.items.length; i++){
+  
+  select.options[select.options.length] = new Option(restaurants.results.items[i].title, restaurants.results.items[i].title)
+}
+ select.addEventListener('change', changeOption)
 
-    contenido.innerHTML += `
-    <tr>
-      <td> ${restaurants.results.items[0].title}</td>
-      <td>${restaurants.results.items[0].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[1].title}</td>
-      <td>${restaurants.results.items[1].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[2].title}</td>
-      <td>${restaurants.results.items[2].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[3].title}</td>
-      <td>${restaurants.results.items[3].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[4].title}</td>
-      <td>${restaurants.results.items[4].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[5].title}</td>
-      <td>${restaurants.results.items[5].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[6].title}</td>
-      <td>${restaurants.results.items[6].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[7].title}</td>
-      <td>${restaurants.results.items[7].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[8].title}</td>
-      <td>${restaurants.results.items[8].vicinity}</td>
-    </tr>
-    <tr>
-      <td> ${restaurants.results.items[9].title}</td>
-      <td>${restaurants.results.items[9].vicinity}</td>    
-    </tr>
-  `
+/*function changeOption(){ NO SELECCIONA OPCION
+  food = select.selectedIndex
+  console.log(food)
+}
+  changeOption();*/
+
            xhr.open()
          }).catch(function (err) {
          })
-       
         
-        }         
+          
         
     
